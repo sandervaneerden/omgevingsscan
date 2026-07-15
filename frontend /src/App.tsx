f6a16de4bc3071c44fe5c5f0a81cbd5de8874889
sandervaneerden <sander.vaneerden@vrd.nl>
2026-07-15 04:22:42 +0200

    Create App.tsx

---
+++ frontend /src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+
+    <div className="app">
+
+      <h1>
+        Omgevingsscan
+      </h1>
+
+      <p>
+        Eerste MVP versie actief.
+      </p>
+
+    </div>
+
+  );
+
+}
+
+export default App;
